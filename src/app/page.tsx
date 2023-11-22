acdf657ab253cdb217b281ebad58b2bc9294d71f
Vincent Franstyo <sorakiriko28814@gmail.com>
2023-11-22 19:13:57 +0700

    chore(cleanup): ig cleanup a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,49 @@
-{/**Home Page and Landing Page */}
+{/**Home Page and Landing Page */
+}
 import Feed from "../components/Feed"
 import {PrismaClient} from '@prisma/client';
-import { User } from '@/db/models'
+import {User} from '@/db/models'
 
 const prisma = new PrismaClient();
 
 export async function users() {
-  const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany();
 
-  // Close the connection to the database
-  await prisma.$disconnect();
+    // Close the connection to the database
+    await prisma.$disconnect();
 
-  return {props: {users}};
+    return {props: {users}};
 }
 
 const Home = () => {
-  // variable to keep the user status
-  const isUserLogged = true;
+    // variable to keep the user status
+    const isUserLogged = true;
 
-  return (
-    <section className="w-full flex-start flex-col mx-16">
-      {isUserLogged?(
-        <div>
-          <h1 className="headline_text text-left blue_gradient">
-          Welcome, Kean!  
-          <br/>
-        </h1>
-        <h2 className="orange_gradient headline_subtext text-left">Where would you like to play?</h2>
-        </div>
-      ):(   //todo implement default page
-        <div>
-          <h1 className="headline_text text-left blue_gradient">
-          Welcome to BaTaGOR
-          <br/>
-        </h1>
-        <h2 className="orange_gradient headline_subtext text-left">Sign In to access our collection of football fields</h2>
+    return (<section className="w-full flex-start flex-col mx-16">
+            {isUserLogged ? (<div>
+                    <h1 className="headline_text text-left blue_gradient">
+                        Welcome, Kean!
+                        <br/>
+                    </h1>
+                    <h2 className="orange_gradient headline_subtext text-left">Where would you like to play?</h2>
+                </div>) : (   // todo implement default page
+                <div>
+                    <h1 className="headline_text text-left blue_gradient">
+                        Welcome to BaTaGOR
+                        <br/>
+                    </h1>
+                    <h2 className="orange_gradient headline_subtext text-left">Sign In to access our collection of
+                                                                               football fields</h2>
 
-        
-        </div>
-        
-      )}
 
-      <Feed />
+                </div>
 
-      
-    </section>
-  )
+            )}
+
+            <Feed/>
+
+
+        </section>)
 }
 
 export default Home
