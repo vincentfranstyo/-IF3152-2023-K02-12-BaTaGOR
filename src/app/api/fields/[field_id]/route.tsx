19324deb3b68abce5089bc059738b9cb36385868
pkmntrainerfrost <smaarif.achsien@gmail.com>
2023-11-26 17:18:41 +0700

    refactor: change api route for field UD ops

--- src/app/api/fields/[field_id]/route.tsx
+++ src/app/api/fields/[field_id]/route.tsx
@@ -4,6 +4,7 @@ import {hash} from "bcrypt";
 import * as z from "zod"
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { NextApiRequest } from "next";
 
 // Define a Field schema for input validation
 
@@ -21,9 +22,24 @@ const FieldSchema = z
     owner_id: z.number().int().optional(),
 })
 
-export async function PUT(req: Request) {
+type Field = {
+    field_id: number;
+    field_name?: string;
+    street?: string;
+    city?: string;
+    province?: string;
+    postal_code?: number;
+    image_url?: string;
+    rate_per_hour?: number;
+    operational_status?: string;
+    owner_id?: number;
+  };
+
+export async function PUT(req: Request, context : { params : {field_id : string}}) {
 
     try {
+
+        console.log(context.params.field_id);
 
         // auth
         const session = await getServerSession(authOptions)
@@ -32,17 +48,21 @@ export async function PUT(req: Request) {
 
         if (session.user.username !== "admin") {
             return NextResponse.json({field: null, message: "Forbidden"}, {status: 403})
-        }
+        } 
 
         // Parse input
         const body = await req.json();
-        const { field_id, field_name, street, city, province, postal_code, image_url, rate_per_hour, operational_status, owner_id } : 
-        { field_id : number, field_name? : string, street? : string, city? : string, province? : string, postal_code? : number, image_url? : string, rate_per_hour? : number, operational_status? : string, owner_id? : number}
-        = FieldSchema.parse(body)
+        const parsedField: Field = FieldSchema.parse(body);
+
+        // Create a new object with the field_id from the context
+        const field: Field = {
+            ...parsedField,
+            field_id: parseInt(context.params.field_id)
+        };
 
         // Check for field in db
         const existingFieldById = await db.field.findUnique({
-            where: {field_id: field_id }
+            where: {field_id: field.field_id }
         })
 
         if (existingFieldById === null) {
@@ -52,22 +72,22 @@ export async function PUT(req: Request) {
         // Update
         const updateField = await db.field.update({
             where: {
-                field_id : field_id,
+                field_id : field.field_id,
             },
             data: {
-                field_name : field_name ?? existingFieldById.field_name,
-                street : street ?? existingFieldById.street,
-                city : city ?? existingFieldById.city,
-                province : province  ??  existingFieldById.province,
-                postal_code : postal_code ??  existingFieldById.postal_code,
-                image_url : image_url  ??  existingFieldById.image_url,
-                rate_per_hour : rate_per_hour  ??  existingFieldById.rate_per_hour,
-                operational_status : operational_status ??  existingFieldById.operational_status,
-                owner_id : owner_id  ??  existingFieldById.owner_id
+                field_name : field.field_name ?? existingFieldById.field_name,
+                street : field.street ?? existingFieldById.street,
+                city : field.city ?? existingFieldById.city,
+                province : field.province  ??  existingFieldById.province,
+                postal_code : field.postal_code ??  existingFieldById.postal_code,
+                image_url : field.image_url  ??  existingFieldById.image_url,
+                rate_per_hour : field.rate_per_hour  ??  existingFieldById.rate_per_hour,
+                operational_status : field.operational_status ??  existingFieldById.operational_status,
+                owner_id : field.owner_id  ??  existingFieldById.owner_id
             },
         })
 
-        return NextResponse.json({field: updateField, message: "Field updated successfully"}, {status : 201});
+        return NextResponse.json({field: updateField, message: "Field updated successfully"}, {status : 200});
 
     } catch (error) {
         console.error("Error", error)
@@ -76,7 +96,7 @@ export async function PUT(req: Request) {
 
 }
 
-export async function DELETE(req: Request) {
+export async function DELETE(req: Request, context : { params : {field_id : string}}) {
 
     try {
 
@@ -87,13 +107,10 @@ export async function DELETE(req: Request) {
 
         if (session.user.username !== "admin") {
             return NextResponse.json({field: null, message: "Forbidden"}, {status: 403})
-        }
+        } 
 
         // Parse input
-        const body = await req.json();
-        const { field_id } : 
-        { field_id : number }
-        = FieldSchema.parse(body)
+        const field_id = parseInt(context.params.field_id)        
 
         // Check for field in db
         const existingFieldById = await db.field.findUnique({
@@ -111,7 +128,7 @@ export async function DELETE(req: Request) {
             },
           })
 
-        return NextResponse.json({field: existingFieldById, message: "Field updated successfully"}, {status : 201});
+        return NextResponse.json({field: existingFieldById, message: "Field deleted successfully"}, {status : 200});
 
     } catch (error) {
         console.error("Error", error)
